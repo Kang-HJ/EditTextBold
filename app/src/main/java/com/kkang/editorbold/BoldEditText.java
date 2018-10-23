568066b54379186690ea919d6a3d1a9fd05c60df
강현정 <kkang9915@moolban.com>
2018-10-23 20:55:32 +0900

    BoldEditText 상속 클래스 생성

--- app/src/main/java/com/kkang/editorbold/BoldEditText.java
+++ app/src/main/java/com/kkang/editorbold/BoldEditText.java
@@ -1,15 +1,40 @@
 package com.kkang.editorbold;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.util.AttributeSet;
 import android.widget.EditText;
 
+@SuppressLint("AppCompatCustomView")
 public class BoldEditText extends EditText {
+
+    onSelectionChangedListener listener;
+
     public BoldEditText(Context context) {
         super(context);
+    }
+    public BoldEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BoldEditText(Context context, onSelectionChangedListener listener) {
+        super(context);
+        this.listener = listener;
+    }
+
+    public void setListener(onSelectionChangedListener listener){
+        this.listener = listener;
     }
 
     @Override
     protected void onSelectionChanged(int selStart, int selEnd) {
         super.onSelectionChanged(selStart, selEnd);
+        if (listener != null) {
+            listener.onSelectionChanged(selStart, selEnd);
+        }
+    }
+
+    public interface onSelectionChangedListener {
+        void onSelectionChanged(int selStart, int selEnd);
     }
 }
